d1d5f4dd6a61cb9bd9d90dd1abc41edcfb4b9478
Adair Daniels <andeleidun@gmail.com>
2026-08-25 23:44:45 -0700

    refactor data fetching for explicit errors and parallel requests

--- src/Utilities/fetchPokemon.js
+++ src/Utilities/fetchPokemon.js
@@ -1,55 +1,41 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-function fetchData(url) {
-  // returns a promise with resolve and reject
-  // based on the response status of the fetch request
-  return new Promise(async function (resolve, reject) {
-    const response = await fetch(url);
-    if (response.status === 200) {
-      resolve(response.json());
-    } else {
-      reject("Response status: ", response.status);
-    }
-  });
-}
+async function fetchJson(url) {
+  const response = await fetch(url);
 
-function capitalize(string) {
-  // capitalize the first letter of a string
-  return string[0].toUpperCase() + string.slice(1);
-}
-
-export async function fetchPokePages(page) {
-  // fetches data from primary apiUrl
-  // then fetches further data as pages are called
-  const url = page.pageParam ? page.pageParam : apiUrl;
-  const res = await fetch(url);
-  return res.json();
-}
-
-export async function fetchPokemonByUrls(query) {
-  // fetches detailed Pokemon data based on initial
-  // query results
-  let urls = [];
-  query.queryKey[1].forEach(arr => arr.forEach(item => urls.push(item)));
-  let returnResults = [];
-  let promises = [];
-  urls.forEach((url) => {
-    promises.push(
-      fetchData(url)
-        .then((detailedData) => detailedData)
-        .catch((message) => console.error(message))
-    );
-  })
-  for await (const pokePromise of promises) {
-    returnResults.push({
-      pokedexNumber: pokePromise.id,
-      name: capitalize(pokePromise.name),
-      height: pokePromise.height / 10,
-      weight: pokePromise.weight / 10,
-      types: pokePromise.types.map((type) => type.type.name),
-      sprite: pokePromise.sprites.front_default,
-      shiny: pokePromise.sprites.front_shiny,
-    });
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
   }
-  return returnResults;
+
+  return response.json();
+}
+
+function capitalize(value) {
+  if (!value) return "";
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
+
+function normalizePokemon(pokemon) {
+  return {
+    id: pokemon.id,
+    pokedexNumber: pokemon.id,
+    name: capitalize(pokemon.name),
+    height: pokemon.height / 10,
+    weight: pokemon.weight / 10,
+    types: pokemon.types.map(({ type }) => type.name),
+    sprite: pokemon.sprites.front_default,
+    shiny: pokemon.sprites.front_shiny,
+  };
+}
+
+export async function fetchPokePages({ pageParam = apiUrl }) {
+  return fetchJson(pageParam);
+}
+
+export async function fetchPokemonByUrls({ queryKey }) {
+  const [, urls = []] = queryKey;
+  const uniqueUrls = [...new Set(urls)];
+  const pokemon = await Promise.all(uniqueUrls.map((url) => fetchJson(url)));
+
+  return pokemon.map(normalizePokemon);
 }
